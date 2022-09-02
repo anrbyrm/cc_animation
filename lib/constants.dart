e840030157f82bc457f707cdc8f0449bdb61a498
anrbyrm <anrbyrm@gmail.com>
2022-09-02 17:23:56 +0400

    constants updated

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,13 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-const double horizontalPadding = 20;
-const double verticalPadding = 50;
+double horizontalPadding = 25.w;
+double verticalPadding = 40.h;
+
+double cardBorderRadius = 8.h;
 
 const int animationDuration = 1000;
 
 const scaffoldColor = Color(0xFF040303);
+
+const calendarCurve = Curves.easeInOutBack;
